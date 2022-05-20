6e07f9c7c404e84301d47cf70dc59fa052476ef3
DVDaie <105940928+DVDaie@users.noreply.github.com>
2022-05-20 19:45:08 +0200

    Update and rename config-template.php to config.php

--- src/config.php
+++ src/config.php
@@ -11,12 +11,12 @@
  */
 
 /*  Constants for telegram API */
-define('TELEGRAM_BOT_TOKEN', '');
+define('TELEGRAM_BOT_TOKEN', '5391883429:AAEmroiJZeOyko8KCLfFNnrrRk780MG9kUk');
 define('TELEGRAM_API_URI_BASE', 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/');
 define('TELEGRAM_FILE_API_URI_BASE', 'https://api.telegram.org/file/bot' . TELEGRAM_BOT_TOKEN . '/');
 
 /*  Constants for DB Access */
-define('DATABASE_HOST', '');
+define('DATABASE_HOST', 'https://github.com/CodeMOOC/TreasureHuntBot/blob/feature/docker');
 define('DATABASE_NAME', '');
 define('DATABASE_USERNAME', '');
 define('DATABASE_PASSWORD', '');
@@ -28,7 +28,7 @@ define('CHAT_GROUP_DEBUG', 0);
 define('DEBUG_TO_DB', false);
 define('DEBUG_TO_BOT', false);
 define('DEACTIVATED', false);
-define('BOT_DEEPLINK_START_ROOT', 'https://t.me/BOTNAME?start=');
+define('BOT_DEEPLINK_START_ROOT', 'https://t.me/Caccia_Tesoro_55_bot?start=');
 
 // PHP configuration
 date_default_timezone_set('UTC'); // ensure UTC is used for all date functions
